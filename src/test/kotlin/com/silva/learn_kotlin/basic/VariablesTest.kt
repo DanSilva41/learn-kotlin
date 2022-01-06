3beb90cf8309ec44db0acb5a00814084167e6d71
Danilo da Silva Pereira <t0305dnl@prestadorcbmp.com.br>
2022-01-06 10:32:58 -0300

    Learning about snap two variables

--- src/test/kotlin/com/silva/learn_kotlin/basic/VariablesTest.kt
+++ src/test/kotlin/com/silva/learn_kotlin/basic/VariablesTest.kt
@@ -8,4 +8,13 @@ internal class VariablesTest {
     fun shouldCallRun() {
         Variables().run()
     }
+
+    @Test
+    fun shouldShowSnapVariables() {
+        var a = 10
+        var b = 20
+        b = a.also { a = b }
+        println(a)
+        println(b)
+    }
 }
