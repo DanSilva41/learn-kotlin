64c2bfdffdaed67e416db3915b3b1316b8ee7d3f
Danilo da Silva Pereira <t0305dnl@prestadorcbmp.com.br>
2022-01-06 16:11:40 -0300

    Lambdas expressions

---
+++ src/test/kotlin/com/silva/learn_kotlin/functions/LambdasTest.kt
@@ -0,0 +1,37 @@
+package com.silva.learn_kotlin.functions
+
+import org.junit.jupiter.api.Test
+
+internal class LambdasTest {
+
+    @Test
+    fun shouldShowFoldFunction() {
+        val items = listOf(1, 2, 3, 4, 5)
+
+        items.fold(0, { acc, i ->
+            print("acc = $acc, i = $i")
+            val result = acc + i
+            println("result = $result")
+            result
+        })
+
+        println(items.fold(1, Int::times))
+    }
+
+    @Test
+    fun shouldTestFunctionTypeInstance() {
+        val a = { i: Int -> i + 1 }
+        println(a(3))
+
+        val stringPlus: (String, String) -> String = String::plus
+        val intPlus: Int.(Int) -> Int = Int::plus
+
+        println(stringPlus.invoke("<-", "->"))
+        println(stringPlus("Hello, ", "world!"))
+
+        println(intPlus.invoke(1, 2))
+        println(intPlus(1, 1))
+        println(2.intPlus(3)) // extension
+
+    }
+}
