f00b196ac9c8e7726ffe606444829f28fed43904
Danilo da Silva Pereira <t0305dnl@prestadorcbmp.com.br>
2022-01-06 13:10:09 -0300

    Extension functions

--- src/test/kotlin/com/silva/learn_kotlin/functions/ObjectExpressions.kt
+++ src/test/kotlin/com/silva/learn_kotlin/functions/ObjectExpressions.kt
@@ -15,4 +15,17 @@ internal class ObjectExpressions {
 
         println(helloWorldObject)
     }
+
+    @Test
+    fun shouldCreateExtensionFunction() {
+        val list = mutableListOf(1, 2, 3)
+        list.swap(0, 2)
+        println(list)
+    }
+
+    fun MutableList<Int>.swap(index1: Int, index2: Int) {
+        val tmp = this[index1]
+        this[index1] = this[index2]
+        this[index2] = tmp
+    }
 }
